08e1fd20a32198df0961085ff195a4cf9a34647c
Henil Patel <sujjugaming02@gmail.com>
2025-05-17 17:02:02 +0530

    all done

--- server.js
+++ server.js
@@ -84,7 +84,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server running at http://0.0.0.0:${port}`);
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
-
